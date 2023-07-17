13bb8ea07810d1be4c969cf5fb8c2f67719fbca8
Aridany <aridany54@gmail.com>
2023-07-17 04:07:50 +0100

    Images and first steps with Sass

---
+++ build/js/api-tacos.js
@@ -0,0 +1,31 @@
+async function Datos(){
+    /*
+    const url = 'https://the-mexican-food-db.p.rapidapi.com/';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '33f14c8e40msh5c81a768c0e3502p10255djsn5221b3fed65c',
+            'X-RapidAPI-Host': 'the-mexican-food-db.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+    */
+
+/*
+    const URL = 'https://the-mexican-food-db.p.rapidapi.com/';
+
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    })
+    */
+}
