74d0903da70913e7d49b3acc25616fef711138a2
Aridany <aridany54@gmail.com>
2023-07-27 09:29:46 +0100

    submenu changes v3

--- build/js/submenu.js
+++ build/js/submenu.js
@@ -4,6 +4,17 @@
 function submenu() {
     document.querySelector('.submenu').style.opacity = "1";
     document.querySelector('.submenu').style.right = "0";
+    //document.querySelector('.submenu').style.animation = "translateX(-1%)";
+
+    //Para el submenu entero
+    const expandElement = document.querySelector('.submenu');
+    expandElement.classList.add('expand');
+
+    //Para la lista
+    document.addEventListener('DOMContentLoaded', function() {
+        const fadeList = document.getElementsByClassName('ul');
+        fadeList.classList.add('show');
+      });
 
    // busqueda.addEventListener('click').style.opacity = "1"
 }
