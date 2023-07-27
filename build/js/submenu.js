b24e821e31a0fb766b454e37080199109e61220a
Aridany <aridany54@gmail.com>
2023-07-27 08:36:21 +0100

    Cambios en el submenu

---
+++ build/js/submenu.js
@@ -0,0 +1,14 @@
+"use strict"
+/*
+//Abrir
+function submenu() {
+    document.querySelector('.submenu').style.opacity = "1";
+    document.querySelector('.submenu').style.right = "0";
+
+   // busqueda.addEventListener('click').style.opacity = "1"
+}
+//Cerrar
+function submenuC(){
+    document.querySelector('.submenu').style.opacity = "0";
+}
+*/
